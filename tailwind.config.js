0e8605303495fc9b32541efb1c106aeb4f2fee37
olzhasar <o.arystanov@gmail.com>
2021-09-11 17:01:37 +0600

    Set-up ubuntu fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,9 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
-      sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
+      sans: ["Ubuntu", ...defaultTheme.fontFamily.sans],
       serif: [...defaultTheme.fontFamily.serif],
-      mono: [...defaultTheme.fontFamily.mono],
+      mono: ["Ubuntu Mono", ...defaultTheme.fontFamily.mono],
     },
     extend: {},
   },
